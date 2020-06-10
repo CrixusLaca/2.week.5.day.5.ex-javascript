b5ef7ef26db5bb53c49853b8d11e477673b8030d
BristyanLaszlo <bristyan.laszlo@gmail.com>
2020-06-10 20:49:21 +0200

    3

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//EXERCISE 1 :
+$('#box1').text('Macrotis');
+$('#box2').css('border-color', 'purple');
+$('#box1').css('background-color', 'yellow');
+$('#box2').css('background-color', 'yellow');
+$('#box3').css('background-color', 'yellow');
+
+//EXERCISE 2:
+$('#color').css('background-color', 'purple');
+
+$('div2').addClass("important");
+
+$('#word').text('Power of DOM');
